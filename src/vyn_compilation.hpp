b77e4e61c5a311f261f88c8eba42cae65b408d8c
Vaenyx <242270953+Vaenyx@users.noreply.github.com>
2026-04-13 18:07:50 +0200

    Added c code formatting for c option

--- src/vyn_compilation.hpp
+++ src/vyn_compilation.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
 #include "generation.hpp"
+#include "helpers.hpp"
 #include "parser.hpp"
 #include "tokenization.hpp"
 
-#include <algorithm>
 #include <cstdlib>
+#include <filesystem>
 #include <iostream>
 #include <string>
 
@@ -25,4 +26,33 @@ std::string vyn_to_c(const std::string &input) {
 
   Generator generator(tree);
   return generator.gen_prog();
+}
+
+std::string format_c(const std::string &c_code) {
+  std::filesystem::path tmp = get_tmp_path("c");
+  write_to_file(tmp, c_code);
+
+  std::string cmd = "clang-format " + tmp.string() + " -i";
+
+  std::system(cmd.c_str());
+
+  std::string formatted_c_code = get_file_content(tmp.string());
+  return formatted_c_code;
+}
+
+void compile_c(std::string c_code, std::string output_path) {
+  std::filesystem::path tmp = get_tmp_path("c");
+
+  write_to_file(tmp, c_code);
+
+  std::string cmd = "clang \"" + tmp.string() + "\" -o \"" + output_path + "\"";
+
+  int result = std::system(cmd.c_str());
+
+  if (result != 0) {
+    std::cerr << "Compilation failed\n";
+    exit(EXIT_FAILURE);
+  }
+
+  std::filesystem::remove(tmp);
 }
